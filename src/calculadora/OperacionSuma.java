764f7b74a34a76c13818d43bd270e720769eb0e9
dani08722 <mejias.lodan24@triana.salesianos.edu>
2025-11-24 14:10:59 +0100

    Clase operacion_suma modificada y principal modificado

--- src/calculadora/OperacionSuma.java
+++ src/calculadora/OperacionSuma.java
@@ -2,7 +2,9 @@ package calculadora;
 
 public class OperacionSuma {
 	
-    public static int sumar(int a, int b) {
-        return a + b;
-    }
+	public static int sumar(int a, int b) {
+	    System.out.println("Realizando la operación de suma...");
+	    return a + b;
+	}
+
 }
--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -2,11 +2,14 @@ package calculadora;
 
 public class Principal {
 	
-    public static void main(String[] args) {
-    	
-        int resultado = OperacionSuma.sumar(5, 3);
-        
-        System.out.println("El resultado de la suma es: " + resultado);
-        
-    }
+	public static void main(String[] args) {
+		
+	    System.out.println("Bienvenido a la calculadora simple.");
+	    
+	    int resultado = OperacionSuma.sumar(5, 3);
+	    
+	    System.out.println("El resultado de la suma es: " + resultado);
+	    
+	}
+
 }
